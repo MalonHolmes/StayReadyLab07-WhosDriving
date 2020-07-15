673db3ca3c19d0031b2524ef6d724c253fe5e23c
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-15 18:02:29 -0400

    Everything done except transport()

--- src/main/java/Vehicles/MountainBike.java
+++ src/main/java/Vehicles/MountainBike.java
@@ -1,8 +1,7 @@
 package Vehicles;
-
 import Driving.Bike;
-
 public class MountainBike extends Bike {
+    Integer currTirePressure = 60; // aka PSI should start at the top tire pressue: 60;
     /**
      * The top speed of a mountain bike should be 28.5
      * but for every 1 PSI under the recommended tire
@@ -11,15 +10,15 @@ public class MountainBike extends Bike {
      *
      * @return 28.5 minus any reduction to top speed
      */
-    @Override
     public Double getTopSpeed() {
-        return null;
+        if (currTirePressure < recommendedTirePressure()){
+            return 28.5 - (recommendedTirePressure() - getTirePressure());
+        }
+        return 28.5;
     }
-
-
     /**
-     * transport should calculate the time it takes in
-     * seconds to travel a distance base on the top
+     * transport() should calculate the time it takes in
+     * seconds to travel a distance base at top
      * speed and update the tire pressure. Long trips
      * on a mountain bike will reduce its PSI, so for
      * every 30 miles traveled in a single trip, the
@@ -27,14 +26,14 @@ public class MountainBike extends Bike {
      * the tires register 20 PSI where no more air
      * will be lost on trips.
      *
-     * @param distance - length of travel in miles
+     * @param distanceInMiles - length of travel in miles
      * @return time in seconds to travel distance
      */
-    @Override
-    public Integer transport(Double distance) {
-        return null;
+    public Integer transport(Double distanceInMiles) {
+        Integer timeToTravelInSecs = 0;
+        // TODO
+        return timeToTravelInSecs;
     }
-
     /**
      * Gets the current amount of pressure in PSI
      * in the tires
@@ -43,23 +42,21 @@ public class MountainBike extends Bike {
      */
     @Override
     public Integer getTirePressure() {
-        return null;
+        return currTirePressure;
     }
-
     /**
      * Returns the tires to the recommended PSI
      */
     @Override
     public void inflateTires() {
-
+        currTirePressure = recommendedTirePressure();
     }
-
     /**
      * The Recommended PSI of a mountain bike is 30
      * @return recommended PSI
      */
     @Override
     public Integer recommendedTirePressure() {
-        return null;
+        return 30;
     }
 }
